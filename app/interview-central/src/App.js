0acedd61d7adb8b54efb75b8c9c9db25ed90ae85
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-14 13:42:36 -0500

    Functional search bar. Needs work on asthetics

--- app/interview-central/src/App.js
+++ app/interview-central/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import BigTile from './views/bigtile/bigtile';
 import LogoutBar from './views/topbar/components/logoutbar';
 import PropTypes from 'prop-types';
-
+import _ from 'lodash';
 
 export class App extends Component {
 
@@ -11,13 +11,32 @@ export class App extends Component {
     questions: PropTypes.array.isRequired,
   };
 
+  componentWillMount() {
+    this.state = {
+      searchTerm: '',
+      currentlyDisplayed: this.props.questions,
+    };
+
+    this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    let newlyDisplayed = _.filter(this.props.questions, question => question.name.toLowerCase().includes(event.target.value.toLowerCase()));
+
+    this.setState({
+      searchTerm: event.target.value,
+      currentlyDisplayed: newlyDisplayed,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <LogoutBar/>
         <div className='title'>INTERVIEW CENTRAL</div>
         {/* TODO: SEARCH BAR GOES HERE */}
-        {this.props.questions.map((question, index) =>
+        <input type="text" onChange={this.onInputChange.bind(this)} />
+        {this.state.currentlyDisplayed.map((question, index) =>
           <BigTile
             questionName={question.name}
             questionDescription={question.description}
