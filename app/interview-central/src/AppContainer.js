5486aa20761f534b59dd848c2b1046f7e3ab43b9
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-13 19:05:23 -0500

    Appcontainer WIP

---
+++ app/interview-central/src/AppContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class AppContainer extends Component {
+    
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+
+}
