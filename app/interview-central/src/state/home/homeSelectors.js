321f1787cc4d21802cecd4f65559196022728e33
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-20 16:43:31 -0500

    cleanup selector

--- app/interview-central/src/state/home/homeSelectors.js
+++ app/interview-central/src/state/home/homeSelectors.js
@@ -3,9 +3,6 @@ import _ from 'lodash';
 export const getHomeData = (state) => {
     if (state.questions && state.questions.questions) {
         let questionNames = state.home.mySuite;
-        console.log("State.home.mySuite");
-        console.log(state.home.mySuite);
-        // let questions = _.filter(state.questions.questions.Items, question => questionNames.includes(question.name));
         return {
             mySuite: questionNames,
             recommended: [],
