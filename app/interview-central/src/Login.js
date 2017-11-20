a69125a45351baf960c67f0e30b4493aa91b0768
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-20 16:41:29 -0500

    Login for routing

---
+++ app/interview-central/src/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+
+    componentWillMount() {
+    }
+
+    render() {
+        return (
+            <div>LOGIN</div>
+        );
+    }
+
+}
--- app/interview-central/src/index.js
+++ app/interview-central/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer';
+import Login from './Login';
 import './index.css';
 import { Provider } from 'react-redux';
 import { configure } from './config/config-store';
@@ -10,7 +11,10 @@ const store = configure({});
 ReactDOM.render(
     <Provider store={store}>
         <Router history={createBrowserHistory()}>
-            <Route exact={true} path="/" component={AppContainer}/>
+            <div>
+                <Route path="/login" component={Login}/>        
+                <Route exact={true} path="/" component={AppContainer}/>
+            </div>
         </Router>
     </Provider>,
     document.getElementById('root')
