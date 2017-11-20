fcb0369ec248e8a6268d2308c3bbafa63f5e40f3
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-20 17:07:52 -0500

    Login button

--- app/interview-central/src/Login.js
+++ app/interview-central/src/Login.js
@@ -17,6 +17,9 @@ export default class Login extends Component {
                 <div className="box">
                     <Textbox name="Password" placeholder="Password" />                
                 </div>
+                <div className="loginDiv">
+                    <button className="loginButton">LOGIN</button>
+                </div>
             </div>
         );
     }
