300a963b7a1cd1dbdd7572c054a117cb3053eb0f
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-22 14:44:52 -0500

    Remove junk code

--- app/interview-central/src/state/suites/suitesActions.js
+++ app/interview-central/src/state/suites/suitesActions.js
@@ -1,15 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-// export function getSuite(id) {
-//     return getSuiteAPI(id).then(([response, json]) => {
-//         if (response.status === 200) {
-//             return json
-//         } else {
-//             return {};
-//         }
-//     });
-// }
-
 export function getSuite(id) {
     const URL = `http://localhost:5000/getSuite/${id}`;
     return fetch(URL, { method: 'GET'});
