78926a2e3ba1f2747cf80f302808e7fb613ecf01
Nikhil Kulkarni <nikhil896@gmail.com>
2017-11-22 16:23:11 -0500

    Stylized more info

--- app/interview-central/src/SuiteContainer.js
+++ app/interview-central/src/SuiteContainer.js
@@ -25,7 +25,6 @@ export class SuiteCont extends Component {
     }
 
     render() {
-
         return (
             <div>
                 <Suite suite={this.state.suite}/>
--- app/interview-central/src/state/home/homeActions.js
+++ app/interview-central/src/state/home/homeActions.js
@@ -25,7 +25,6 @@ export function getHomeDataError() {
 
 export function getHomeDataAction() {
     return (dispatch) => {
-        // dispatch(getQuestions());
         return getHomeDataAPI().then(([response, json]) => {
             if (response.status === 200) {
                 dispatch(getHomeDataSuccess(json));                
